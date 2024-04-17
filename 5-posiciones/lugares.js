78700246bfbb9d6721e8b2a11295c41786d96b24
Matias Suarez <mssuareez7@gmail.com>
2024-04-17 08:34:08 -0300

    Ejercicio 5 con posiciones

--- 5-posiciones/lugares.js
+++ 5-posiciones/lugares.js
@@ -7,10 +7,10 @@ Y a su lado, te estrecha la mano el mediocampista Federico Valverde, conocido co
 Ya es dificil llevar registro de todos los nombres nuevos.
 
 Escriba un programa que:
-- guarde en una colección
-  - que acepte una fecha de nacimiento en formato string
-  - y que devuelva la cantidad de años cumplidos
-- imprima en consola un mensaje en el formato: {nombre} "{apodo}" {apellido} (edad).
+- guarde en una colección información de los jugadores hasta el momento presentados
+- imprima la información de los jugadores en el formato: {nombre} "{apodo}" {apellido} (edad) - {posicion}.
+
+¿Cómo puedo obtener de la colección solamente los que son mediocampistas?
 
 Tip: consulten los métodos de la clase Date
 https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Date
@@ -39,17 +39,28 @@ const jugadores = [
         nombre: "Nahitan",
         apellido: "Nández",
         apodo: "",
-        fechaNacimiento: "1995-12-28"
+        fechaNacimiento: "1995-12-28",
+        posicion: "Mediocampista"
     },
     {
         nombre: "Federico",
         apellido: "Valverde",
         apodo: "Pajarito",
-        fechaNacimiento: null
+        fechaNacimiento: null,
+        posicion: "Mediocampista"
+    },
+    {
+        nombre: "Jayson",
+        apellido: "Tatum",
+        apodo:"Jt",
+        fechaNacimiento:"1998-03-03",
+        posicion:"alero"
     }
 ];
 
 jugadores.forEach(jugador => {
     const edad = calcularEdad(jugador.fechaNacimiento);
-    console.log(`${jugador.nombre} "${jugador.apodo}" ${jugador.apellido} (${edad})`);
+    if (jugador.posicion === "mediocampista" || jugador.posicion === "Mediocampista") {
+        console.log(`${jugador.nombre} "${jugador.apodo}" ${jugador.apellido} (${edad}) ${jugador.posicion}`);
+    }
 });
